8011bd302fdf756fcd1b6c5b8a8aa059209f3c1d
gamhpang <gamhpang1812@gmail.com>
2024-05-21 13:38:31 -0500

    added cors

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ const app = express();
 const songRouter = require('./routes/songsRouter');
 const userRouter = require('./routes/usersRouter');
 const port = 3000;
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 app.use('/songs',songRouter);
 app.use('/users',userRouter);
 
